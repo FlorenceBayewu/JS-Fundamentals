a180828941d228eb2774e5255d4ee3879affa919
Florence Bayewu <florencebayewu2020@gmail.com>
2025-11-28 20:28:16 +0100

    arghhh. commit again

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,6 +1,7 @@
 let message = ["C is fun", "Python is cool", "JavaScript is amazing"];
+//initialise outputStrinh
 let outputString = "";
 for (let i = 0; i < message.length; i++) {
     outputString += message[i] + "\n";
 }
-console.log(outputString)  
+console.log(outputString);
